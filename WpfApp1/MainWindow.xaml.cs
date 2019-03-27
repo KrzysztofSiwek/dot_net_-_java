8474e724c0fa6517862f2b2753d1d2ae0538cde2
robij1996 <robert.stanik1996@gmail.com>
2019-03-27 20:02:53 +0100

    Download Time

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,23 +13,71 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HtmlAgilityPack;
+using System.Threading;
+using System.Net;
+
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
- public MainWindow()
+        public MainWindow()
         {
             InitializeComponent();
         }
-      
-        private void Btn1_Click(object sender, RoutedEventArgs e)
+
+        private async void Btn1_Click(object sender, RoutedEventArgs e)
         {
             img1.Source = new BitmapImage(new Uri(txtBx3.Text, UriKind.Relative));
-            viewBox.Text += "\n" + txtBx1.Text + " " + txtBx2.Text;
+            viewBox.Text += txtBx1.Text + " " + txtBx2.Text + "\n";
+        }
+
+        private async void GetTimeZone_Click(object sender, RoutedEventArgs e)
+        {
+            string data = await TimeZoneConnection.LoadTimeZoneAsync(continent.Text, city.Text);
+            string[] tmp = Regex.Split(data, @"\s");
+            viewBox.Text += city.Text + ": " + tmp[1] +  "\n";
+        }
+
+
+        bool clik = false;
+        System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
+        private string DownloadDataFromSite()
+        {
+
+            var webSet = new HtmlWeb();
+            var doc = webSet.Load("https://24timezones.com/pl_zegar/poland_czas.php");
+            HtmlNode hours = doc.DocumentNode.SelectSingleNode("//span[@class='hours']");
+            HtmlNode minutes = doc.DocumentNode.SelectSingleNode("//span[@class='minutes']");
+            HtmlNode seconds = doc.DocumentNode.SelectSingleNode("//span[@class='seconds']");
+            if (hours != null)
+            {
+                return hours.InnerText + ":" + minutes.InnerText + ":" + seconds.InnerText;
+            }
+            else
+            {
+                return "nic";
+            }
+
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clik = false;
+
+            while (!clik)
+            text_html.Text = await Task.Run(() => DownloadDataFromSite());
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            clik = true;
         }
     }
 }
